85f1f76a31c876a9dfa27906a532af7c583ea24d
Bishal Giri <bsaal2@Bishals-MacBook-Pro.local>
2022-04-29 13:20:48 +0545

    feat: completed the application

--- bustop.py
+++ bustop.py
@@ -1,49 +1,55 @@
+# Necessary variables
 busCapacity = 47
 totalPassengerInBus = 0
 happyCustomer = 0
 unhappyCustomer = 0
 ratio = 0
-routeNumber = 0
-noOfStops = 0
 
+
+# Ask route number and validate it
+routeNumber = 0
 while True:
-    routeNumber = input("Enter the route number :")
     try:
-        routeNumber = int(routeNumber);
+        routeNumber = input("Enter the route number :")
+        routeNumber = int(routeNumber)
         if routeNumber > 0:
-            break;
+            break
         else:
             raise Exception("number should be greater than 0")
-    except ValueError:
+    except:
         print("Please enter a valid number")
 
+
+# Ask the number of bus stop and validate it
+noOfStops = 0
 while True:
-    noOfStops = input("Please enter the number of stops on this route :")
     try:
-        noOfStops = int(noOfStops);
+        noOfStops = input("Please enter the number of stops on this route :")
+        noOfStops = int(noOfStops)
         if noOfStops > 0:
-            break;
+            break
         else:
             raise Exception("Stop should be greater than 0")
     except ValueError:
         print("Please enter a valid number")
 
-print('-------------------------------------------------------------------------------------------')
 
+# Handling the customer joining and leaving the bus
 i = 0
 while i < noOfStops:
 
+    # First bus stop
+    # Customer only joins. They do not leave the bus
     if i == 0:
-
         while True:
-            totalNumber = input(' How many passengers were waiting for the bus at stop #' + str(i + 1) + " : ")
             try:
-                totalNumber = int(totalNumber);
+                totalNumber = input(' How many passengers were waiting for the bus at stop #' + str(i + 1) + " : ")
+                totalNumber = int(totalNumber)
 
-                if totalNumber < 0:
+                if totalNumber < 1:
                     raise Exception("Should be positive integer")
 
-                if totalNumber > 47:
+                if totalNumber > busCapacity:
                     totalPassengerInBus = busCapacity
                     unhappyCustomer = totalNumber - busCapacity
                     happyCustomer = busCapacity
@@ -56,17 +62,20 @@ while i < noOfStops:
             except:
                 print("Enter a valid number")
 
-    if i == noOfStops - 1:
-
+    # This is the last stop
+    # Customer only leave the bus here
+    elif i == noOfStops - 1:
         while True:
-            response = input("How many passengers left the bus at stop # " + str(i + 1) + " : ");
             try:
+                response = input("How many passengers left the bus at stop # " + str(i + 1) + " : ")
                 response = int(response)
-                if response < 0:
+                if response < 1:
                     raise Exception("Number should be greater than 0")
-                if response > 47:
+
+                if response > busCapacity:
                     raise Exception("CANNOT HAVE MORE THAN BUS CAPACITY ")
-                if response <= totalPassengerInBus:
+
+                if response != totalPassengerInBus:
                     raise Exception("All the passenger" + totalPassengerInBus + "should leave bus in last stop")
 
                 totalPassengerInBus = 0
@@ -74,6 +83,8 @@ while i < noOfStops:
                 break
             except:
                 print("Enter a valid number")
+    
+    # This is the bus stop between first and last stop
     else:
         while True:
             try:
@@ -81,13 +92,15 @@ while i < noOfStops:
                 response = int(response)
                 if response < 0:
                     raise Exception("Should be a positive integer")
+
                 if response > busCapacity:
-                    raise Exception("Bus Capacity should be lesser")
+                    raise Exception("Value should not be greater than bus capacity")
+
                 if response > totalPassengerInBus:
                     raise Exception("There cannot more people leaving then they are inside the bus")
 
                 totalPassengerInBus = totalPassengerInBus - response
-                break;
+                break
             except:
                 print("Enter a valid number")
 
@@ -95,31 +108,33 @@ while i < noOfStops:
             try:
                 response = input("How many passengers are joining the bus at stop #" + str(i + 1))
                 response = int(response)
-                if response > 0:
+                if response < 1:
                     raise Exception("Should be a positive integer")
+
                 if response > busCapacity:
                     raise Exception("Bus Capacity should be lesser")
+
                 if (response + totalPassengerInBus) > busCapacity:
                     unhappyCustomer += response + totalPassengerInBus - busCapacity
                     happyCustomer += busCapacity - totalPassengerInBus
-                    totalPassengerInBus = totalPassengerInBus - response
+                    totalPassengerInBus = busCapacity
                 else:
                     happyCustomer += response
                     totalPassengerInBus += response
                 break
             except:
                 print("Enter a valid number")
+        
         i = i + 1
 
-    print('------------------------------------------------------------------------------------')
-    print('                                  RESULT                                             ')
-    print('-------------------------------------------------------------------------------------')
-    print("route number                                    :    " + str(routeNumber))
-    print("Happy customer                                  :    " + str(happyCustomer))
-    print("Unhappy customer                                :    " + str(unhappyCustomer))
+print('------------------------------------------------------------------------------------')
+print('                                  RESULT                                             ')
+print('-------------------------------------------------------------------------------------')
+print("Route number                                    :    " + str(routeNumber))
+print("Happy customer                                  :    " + str(happyCustomer))
+print("Unhappy customer                                :    " + str(unhappyCustomer))
+if unhappyCustomer > 0:
     print("Ratio                                            :      " + str(happyCustomer / unhappyCustomer))
-    # print("Total no of passenger                           : " + str(totalPassengerInBus))
-    # print("Total no of passenger who drop in last route    : " + str(totalPassengerInBus - passengerDrop))
-    #
-    # print("total no of stops                               : " + str(noOfStops))
-    print('-------------------------------------------------------------------------------------')
+else:
+    print("Ratio                                            :      " + str(0.00))
+print('-------------------------------------------------------------------------------------')
